c06013c1b18957f7d5a1fd521761724e5f7dc3aa
Fadel <fadeljesus@gmail.com>
2023-08-08 07:38:32 -0400

    Cap 28 - Ordenando productos

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -6,6 +6,47 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select '.product', 3
+    assert_select '.category', 3
+  end
+
+  test 'render a list of products filtered by category' do
+    get products_path(category_id: categories(:computers).id)
+
+    assert_response :success
+    assert_select '.product', 1
+    assert_select '.category', 3
+  end
+
+  test 'render a list of products filtered by min_price and max_price' do
+    get products_path(min_price: 160, max_price: 200)
+
+    assert_response :success
+    assert_select '.product', 1
+    assert_select 'h2', 'Nintendo Switch'
+  end
+
+  test 'search a product filtered by query_text' do
+    get products_path(query_text: 'switch')
+
+    assert_response :success
+    assert_select '.product', 1
+    assert_select '.category', 3
+  end
+
+  test 'sort products by expensive prices first' do
+    get products_path(order_by: 'expensive')
+
+    assert_response :success
+    assert_select '.product', 3
+    assert_select '.products .product:first-child h2', 'Macbook Air'
+  end
+
+  test 'sort products by cheapest prices first' do
+    get products_path(order_by: 'cheapest')
+
+    assert_response :success
+    assert_select '.product', 3
+    assert_select '.products .product:first-child h2', 'PS4 Fat'
   end
 
   test 'render a detail product page' do
