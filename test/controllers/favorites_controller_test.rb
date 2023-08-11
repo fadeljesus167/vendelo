3f68a893f4f2b14944641f6112777fe9c5d979d9
Fadel <fadeljesus@gmail.com>
2023-08-11 17:02:20 -0400

    Cap 41 - Favoritos parte 2

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     login
     @product = products(:megadrive)
+    @switch = products(:switch)
   end
 
   test "should create favorite" do
@@ -12,5 +13,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     
     assert_redirected_to product_path(@product)
+  end
+
+  test "should unfavorite" do
+    assert_difference('Favorite.count', -1) do
+      delete favorite_url(@switch.id)
+    end
+    
+    assert_redirected_to product_path(@switch)
   end
 end
