8e11ab424e169541007ac95f14fe5d8c27a5e82f
Fadel <fadeljesus@gmail.com>
2023-08-14 16:56:28 -0400

    Cap 52 - ServiceObject

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_202529) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_14_200030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_202529) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,12 +1,16 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
+  def setup
+    @user = User.last
+  end
+  
   test "welcome" do
-    mail = UserMailer.welcome
-    assert_equal "Welcome", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = UserMailer.with(user: @user).welcome
+    assert_equal "Bienvenido a Vendelo", mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ["no-reply@vendelo.com"], mail.from
+    assert_match "Hey #{@user.username}, bienvenid@ a Vendelo.", mail.body.encoded
   end
 
 end
