2721df8f23f43391c27e64d41729e29c24972656
Fadel <fadeljesus@gmail.com>
2023-08-15 12:17:13 -0400

    Cap 55 - Turbo Streams

--- test/jobs/fetch_country_job_test.rb
+++ test/jobs/fetch_country_job_test.rb
@@ -4,4 +4,9 @@ class FetchCountryJobTest < ActiveJob::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "enqueue correctly" do
+    assert_enqueued_jobs 0
+    FetchCountryJob.perform_later('1','0.0.0.0')
+    assert_enqueued_jobs 1
+  end
 end
